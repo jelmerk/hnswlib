71652978253f2b2d88440454f00e6dddbfc23fc0
Jelmer Kuperus <jkuperus@ebay.com>
2019-09-14 14:37:02 +0200

    Use same version

--- hnswlib-pyspark/setup.py
+++ hnswlib-pyspark/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name="pyspark_hnsw",
     url="https://github.com/jelmerk/hnswlib",
-    version="0.1",
+    version="0.18",
     zip_safe=True,
     packages=find_packages(),
 )
