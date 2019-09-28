3783d4924b17674c242f09e076c184c9a8b6f745
Jelmer Kuperus <jkuperus@ebay.com>
2019-09-28 21:52:23 +0200

    Add missing assertions to tests in hnswlib-spark module

--- hnswlib-spark/src/test/scala/com/github/jelmerk/knn/spark/hnsw/HnswSpec.scala
+++ hnswlib-spark/src/test/scala/com/github/jelmerk/knn/spark/hnsw/HnswSpec.scala
@@ -3,17 +3,20 @@ package com.github.jelmerk.knn.spark.hnsw
 import com.holdenkarau.spark.testing.DatasetSuiteBase
 import org.apache.spark.SparkConf
 import org.apache.spark.ml.linalg.{Vector, Vectors}
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, OptionValues}
 import org.scalatest.Matchers._
 
 case class VectorInputRow(id: Int, vector: Vector)
+case class VectorOutputRowNeighbor(neighbor: Int, distance: Float)
+case class VectorOutputRow(id: Int, neighbors: Seq[VectorOutputRowNeighbor])
 
 case class ArrayInputRow(id: String, vector: Array[Float])
+case class ArrayOutputRowNeighbor(neighbor: String, distance: Float)
+case class ArrayOutputRow(id: String, neighbors: Seq[ArrayOutputRowNeighbor])
 
-class Person(val name: String) extends Serializable
+class HnswSpec extends FunSuite with DatasetSuiteBase with OptionValues {
 
-class HnswSpec extends FunSuite with DatasetSuiteBase {
-
+  // for some reason kryo cannot serialize the hnswindex so configure it to make sure it never gets serialized
   override def conf: SparkConf = super.conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
 
   test("vector input row") {
@@ -36,24 +39,23 @@ class HnswSpec extends FunSuite with DatasetSuiteBase {
 
     val model = hnsw.fit(input)
 
-    val result = model.transform(input)
+    val result = model.transform(input).as[VectorOutputRow]
+      .collect()
 
-    result.explain()
-
-    result.printSchema()
-
-    result.show()
+    result should have size 3
+    result should contain(VectorOutputRow(2, Seq(VectorOutputRowNeighbor(3, 0.0076490045f), VectorOutputRowNeighbor(1, 0.11621308f))))
+    result should contain(VectorOutputRow(3, Seq(VectorOutputRowNeighbor(2, 0.0076490045f), VectorOutputRowNeighbor(1, 0.06521261f))))
+    result should contain(VectorOutputRow(1, Seq(VectorOutputRowNeighbor(3, 0.06521261f), VectorOutputRowNeighbor(2, 0.11621308f))))
   }
 
-
   test("array input row") {
-
     val sqlCtx = sqlContext
     import sqlCtx.implicits._
 
     val input = sc.parallelize(Seq(
       ArrayInputRow("1", Array(0.0110f, 0.2341f)),
-      ArrayInputRow("2", Array(0.2300f, 0.3891f))
+      ArrayInputRow("2", Array(0.2300f, 0.3891f)),
+      ArrayInputRow("3", Array(0.4300f, 0.9891f))
     )).toDS
 
     val hnsw = new Hnsw()
@@ -65,11 +67,13 @@ class HnswSpec extends FunSuite with DatasetSuiteBase {
 
     val model = hnsw.fit(input)
 
-    val result = model.transform(input)
+    val result = model.transform(input).as[ArrayOutputRow]
+      .collect()
 
-    result.printSchema()
-
-    result.show()
+    result should have size 3
+    result should contain(ArrayOutputRow("2", Seq(ArrayOutputRowNeighbor("3", 0.0076490045f), ArrayOutputRowNeighbor("1", 0.11621308f))))
+    result should contain(ArrayOutputRow("3", Seq(ArrayOutputRowNeighbor("2", 0.0076490045f), ArrayOutputRowNeighbor("1", 0.06521261f))))
+    result should contain(ArrayOutputRow("1", Seq(ArrayOutputRowNeighbor("3", 0.06521261f), ArrayOutputRowNeighbor("2", 0.11621308f))))
   }
 
 }
