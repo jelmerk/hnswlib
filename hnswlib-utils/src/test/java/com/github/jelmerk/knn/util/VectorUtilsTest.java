5296286c9c97c6467affcf5f0c6a030978b0cec7
Jelmer Kuperus <jkuperus@ebay.com>
2019-10-29 18:24:33 +0100

    use the prefered juniper way

--- hnswlib-utils/src/test/java/com/github/jelmerk/knn/util/VectorUtilsTest.java
+++ hnswlib-utils/src/test/java/com/github/jelmerk/knn/util/VectorUtilsTest.java
@@ -6,29 +6,29 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static com.github.jelmerk.knn.util.VectorUtils.*;
 
-public class VectorUtilsTest {
+class VectorUtilsTest {
 
     private float[] floatVector = { 0.01f, 0.02f, 0.03f };
 
     private double[] doubleVector = { 0.01d, 0.02d, 0.03d };
 
     @Test
-    public void calculateFloatVectorMagnitude() {
+    void calculateFloatVectorMagnitude() {
         assertThat(magnitude(floatVector), is(0.037416574f));
     }
 
     @Test
-    public void calculateDoubleVectorMagnitude() {
+    void calculateDoubleVectorMagnitude() {
         assertThat(magnitude(doubleVector), is(0.03741657386773942d));
     }
 
     @Test
-    public void normalizeFloatVector() {
+    void normalizeFloatVector() {
         assertThat(normalize(floatVector), is(new float[] { 0.26726124f, 0.5345225f, 0.8017837f }));
     }
 
     @Test
-    public void normalizeDoubleVector() {
+    void normalizeDoubleVector() {
         assertThat(normalize(doubleVector), is(new double[] { 0.2672612419124244d, 0.5345224838248488d, 0.8017837257372731d }));
     }
 }
