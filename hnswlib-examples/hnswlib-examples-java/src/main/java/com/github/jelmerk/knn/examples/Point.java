094850b0279bbcccac0ed3ef89c3ee014274c64f
Muhammad Ashfaq <ashfaq92@outlook.com>
2019-11-16 19:55:41 +0800

    Create Point.java
    For making random numeric point

---
+++ hnswlib-examples/hnswlib-examples-java/src/main/java/com/github/jelmerk/knn/examples/Point.java
@@ -0,0 +1,43 @@
+package com.github.jelmerk.knn.examples;
+
+import com.github.jelmerk.knn.Item;
+
+import java.util.Arrays;
+
+public class Point implements Item<Integer, double[]> {
+
+    private static final long serialVersionUID = 1L;
+
+    private final Integer id;
+    private final double[] vector;
+
+    Point(Integer id, double[] vector) {
+        this.id = id;
+        this.vector = vector;
+    }
+
+    @Override
+    public Integer id() {
+        return id;
+    }
+
+    @Override
+    public double[] vector() {
+        return vector;
+    }
+
+    @Override
+    public String toString() {
+        return "TestCase{" +
+                "id='" + id + '\'' +
+                ", vector=" + Arrays.toString(vector) +
+                '}';
+    }
+    String printPoint() {
+        return "("+vector[0]+","+vector[1]+")";
+
+    }
+
+
+
+}
